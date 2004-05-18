b7503f5772fcdbbd998f22b622ccb56030f783d5
corydodt <corydodt@9c44f903-d3d9-0310-ac44-eff4510ebdad>
2004-05-18 16:42:25 +0000

    if we're in a tag, straight version string; otherwise, add the revision for trunk

--- trunk/code/pbp/version.py
+++ trunk/code/pbp/version.py
@@ -1,2 +1,8 @@
+import re
+
 version_info = ('0', '2', '1', None)
-version = ".".join(version_info[:3])
+svn_head_url = "$HeadURL$"
+if re.match("/pbp/tags/", svn_head_url):
+    version = ".".join(version_info[:3])
+else:
+    version = ".".join(version_info[:3]) + '+'
