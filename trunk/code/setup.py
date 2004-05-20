e1a74364383e6bbe707ac62b528ab94ee9f67aaf
corydodt <corydodt@9c44f903-d3d9-0310-ac44-eff4510ebdad>
2004-05-20 05:28:12 +0000

    build stuff will include wwwsearch.zip and licenses where appropriate

--- trunk/code/setup.py
+++ trunk/code/setup.py
@@ -1,4 +1,12 @@
 from distutils.core import setup
+from distutils.command.install_data import install_data 
+class smart_install_data(install_data): 
+    def finalize_options (self):
+        self.set_undefined_options('install',
+            ('install_lib', 'install_dir')
+        )
+        install_data.finalize_options(self)
+
 try:
     import py2exe
 except ImportError:
@@ -16,7 +24,10 @@ setup_args = dict(name="pbp", author="Cory Dodt",
                   author_email='corydodt@twistedmatrix.com',
                   url='http://localhost/FIXME',
                   version=version,
-                  scripts=['pbpscript'], packages=['pbp'])
+                  scripts=['pbpscript'], packages=['pbp'],
+                  data_files = [('pbp', ['pbp/wwwsearch.zip',])],
+                  cmdclass = dict(install_data = smart_install_data),
+                  )
 
 
 
