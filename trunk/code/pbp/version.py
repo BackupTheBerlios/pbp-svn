f41b5b6cc94256bef993d3f220e19119f0bff5d2
corydodt <corydodt@9c44f903-d3d9-0310-ac44-eff4510ebdad>
2004-05-21 15:58:03 +0000

    don't need re for checking  version string

--- trunk/code/pbp/version.py
+++ trunk/code/pbp/version.py
@@ -1,8 +1,6 @@
-import re
-
 version_info = ('0', '2', '2', None)
 svn_head_url = "$HeadURL$"
-if re.search("/pbp/tags/", svn_head_url):
+if svn_head_url.find("/pbp/tags/") >= 0:
     version = ".".join(version_info[:3])
 else:
     version = ".".join(version_info[:3]) + '+'
