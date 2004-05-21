adccaf548bde80823a8ce6e799efc3fcf1987413
corydodt <corydodt@9c44f903-d3d9-0310-ac44-eff4510ebdad>
2004-05-21 00:24:29 +0000

    bump version

--- trunk/code/pbp/version.py
+++ trunk/code/pbp/version.py
@@ -1,6 +1,6 @@
 import re
 
-version_info = ('0', '2', '1', None)
+version_info = ('0', '2', '2', None)
 svn_head_url = "$HeadURL$"
 if re.match("/pbp/tags/", svn_head_url):
     version = ".".join(version_info[:3])
