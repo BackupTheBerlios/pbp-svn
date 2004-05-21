5089680a9991bc6af9d9ef4fee5a384afc00e2d7
corydodt <corydodt@9c44f903-d3d9-0310-ac44-eff4510ebdad>
2004-05-21 00:56:26 +0000

    Re-releasing 0.2.2

--- tags/0.2.2/code/buildbot/run_pbpscript.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-TESTDIR=$1
-
-cd $TESTDIR
-for test in *.tests; do
-    bash $test
-done
-
--- tags/0.2.2/code/pbp/version.py
+++
@@ -1,8 +0,0 @@
-import re
-
-version_info = ('0', '2', '2', None)
-svn_head_url = "$HeadURL$"
-if re.search("/pbp/tags/", svn_head_url):
-    version = ".".join(version_info[:3])
-else:
-    version = ".".join(version_info[:3]) + '+'
