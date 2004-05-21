218bf812b5b5be1cf91a1f5bc6c4051894994ebb
corydodt <corydodt@9c44f903-d3d9-0310-ac44-eff4510ebdad>
2004-05-21 00:38:35 +0000

    same as r44

--- trunk/code/pbp/version.py
+++ trunk/code/pbp/version.py
@@ -2,7 +2,7 @@ import re
 
 version_info = ('0', '2', '2', None)
 svn_head_url = "$HeadURL$"
-if re.match("/pbp/tags/", svn_head_url):
+if re.search("/pbp/tags/", svn_head_url):
     version = ".".join(version_info[:3])
 else:
     version = ".".join(version_info[:3]) + '+'
