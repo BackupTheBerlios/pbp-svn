070e5f0173fc653380b280bfa04c943086914067
corydodt <corydodt@9c44f903-d3d9-0310-ac44-eff4510ebdad>
2004-09-15 18:37:14 +0000

    commit granularity changes for buildbot steps

--- trunk/code/Buildbot/pbpstep.py
+++ trunk/code/Buildbot/pbpstep.py
@@ -2,19 +2,22 @@ from buildbot.process import step
 from buildbot.status import event
 
 import re
+import os
 
 class PBP(step.ShellCommand):
     flunkOnFailure = True
     name = "pbp"
 
     def __init__(self, filename=None, **kwargs):
-        if not directory:
-            raise TypeError("please pass .tests filename")
-        command = "bash run_pbpscript.bash %s" % (filename,)
+        if not filename:
+            raise TypeError("please pass a .tests file")
+        command = "python run_pbpscript %s" % (filename,)
+        self.statusName = "pbp %s" % (os.path.basename(filename),)
         step.ShellCommand.__init__(self, command=command, **kwargs)
 
     def startStatus(self):
-        evt = event.Event("yellow", "running pbp".split(),
+        evt_text = "running %s" % (self.statusName,)
+        evt = event.Event("yellow", evt_text.split(),
                           files={'log':self.log})
         self.setCurrentActivity(evt)
 
@@ -27,12 +30,14 @@ class PBP(step.ShellCommand):
         self.failures += len(re.findall('\*\*\* .*FAILURE: (.*)\*\*\*', output))
         self.successes += len(re.findall('\nSUCCESS: ', output))
 
-        result = (step.SUCCESS, [str(self.successes), 'pbp tests', 'passed'])
+
+        result = (step.SUCCESS, [str(self.successes), self.statusName, 'passed'])
 
         total = self.failures + self.successes
 
         if self.failures:
-            result = (step.FAILURE, ['%s/%s' % (self.failures, total), 'pbp', 
+            result = (step.FAILURE, ['%s/%s' % (self.failures, total),
+                                     self.statusName,
                                      'failures'])
 
         return self.stepComplete(result)
@@ -41,7 +46,7 @@ class PBP(step.ShellCommand):
         tot = self.failures + self.successes
         if self.failures:
             color = "red"
-            text = ("%s/%s tests failed" % (self.failures, tot)).split()
+            text = ("%s/%s %s tests failed" % (self.failures, tot, self.statusName)).split()
         else:
             color = "green"
             text = ("%s tests succeeded" % (self.successes)).split()
